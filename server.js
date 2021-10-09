17602d41781cf831273eb997147647ec358971a8
Divesh Kamble <diveshkamble@gmail.com>
2021-10-09 23:07:59 +0530

    Revamped and added Deta

--- server.js
+++ server.js
@@ -1,217 +1,59 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
-const MongoClient  = require('mongodb').MongoClient;
-const dbURL = process.env.DB_URI;
-const dbName = 'UrlShort';
-const urlRegex = new RegExp(/https?:\/\/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi);
-const shortRegex = new RegExp(/([0-9]){4}/gi);
-
-
+const bodyParser = require("body-parser");
+const validurl = require("valid-url");
+const { Deta } = require("deta");
+const deta = Deta(process.env);
+const db = deta.Base("shorturl");
 
 app.use(cors());
 
-//app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-
-
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
-app.get('/api/shorturl/:shorturl?',async function(req,res) {
-  const shorturl = await req.params.shorturl;
-  console.log('shorturl: '+shorturl);
-
-    const shortTest = await shortRegex.test(shorturl);
-
-    if (shortTest)
-   {
-  const data1 = await getOriginalUrl(shorturl)
-
-
-
-
-  if(data1==null)
-  {
-res.json({"error": "No short URL found for the given input"})
-}
-else
-{
-  res.redirect(data1.url);
-}
-
-    }
-else
-{
-
-res.json({error:"Wrong Format"})
-}
-})
-
+app.get("/api/shorturl/:shorturl?", async function (req, res) {
+  const url_id = req.params.shorturl;
+  const get_url = await db.get(url_id);
+  res.redirect(get_url.url);
+});
 
 // Your first API endpoint
-app.post('/api/shorturl', async function(req, res) {
-  let test = urlRegex.test(req.body.url);
-  console.log('url: '+req.body.url);
-  if(test)
-  {
-    //const data = await checkUrl(req.body.url);
-   // if(data){
-
-   //   res.json({original_url:data.url,short_url:data.short})
-   // }
-    //else
-    //{
-   
-      let short = Math.floor(Math.random()*10000+1);
-      //while(await checkShortUrl(short))
-      //{
-      //short = Math.floor(Math.random()*10000+1);
-      //}
-      const insertResult = await insertURL(req.body.url,short);
-      if (insertResult)
-      res.json({original_url:req.body.url,short_url:short});
-    //}
-
-    
+app.post("/api/shorturl", async function (req, res) {
+  const url = req.body.url;
+  //const urlHash = crypto.createHash("md5").update(url).digest("hex");
+  if (!validurl.isUri(url)) return res.json({ error: "Invalid URL" });
+  else {
+    const toInsert = { url };
+    const exists = await db.fetch({ url }, { limit: 1 });
+    console.log(exists);
+    if (exists)
+      res.send(
+        JSON.stringify({
+          original_url: exists.items[0].url,
+          short_url: exists.items[0].key,
+        })
+      );
+    else {
+      const insertedURL = await db.put(toInsert);
+      res.send(
+        JSON.stringify({ original_url: url, short_url: insertedURL.key })
+      );
+    }
   }
-  else
-  {
-    res.json({"error":"invalid url"})
-  }
-   
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
-
-
-
- async function checkUrl(url){
- let client = new MongoClient(dbURL,{ useNewUrlParser: true, useUnifiedTopology: true  })
-let conn = await client.connect()
-  try {
-
-
-    const db = conn.db(dbName);
-
-
-    let r = await db.collection('urlmain').findOne({"url":url});
-     conn.close();
-    return r;
-
-    
-  } catch (err) {
-    console.log(err.stack);
-  }
-
-  // Close connection
- 
-}
-
-
-async function checkShortUrl(short){
-
-   let client = new MongoClient(dbURL,{ useNewUrlParser: true, useUnifiedTopology: true  })
-
-let conn = await client.connect()
-
-  try {
- 
-    const db = conn.db(dbName);
-
-  
-    let r = await db.collection('urlmain').findOne({"short":short});
-
-   if (r===null)
-   {
-  
-   return false;
-   conn.close();
-   }
-   else
-   {
-
-   return true;
-   conn.close();
-   }
-
-    
-  } catch (err) {
-    console.log(err.stack);
-  }
-
-  // Close connection
-  
-
-}
-
-
-async function insertURL(url,short){
-  const client = new MongoClient(dbURL,{ useNewUrlParser: true, useUnifiedTopology: true  })
-const conn = await client.connect()
-  try {
-       const db = conn.db(dbName);
- 
-    let r = await db.collection('urlmain').insertOne({"url":url,"short":short,"date":await new Date()});
-   //console.log(r);
-  if(r.insertedCount!==0)
-  {  // Close connection
-  conn.close();
-    return true;
-  }
-
-  else
-  {  // Close connection
-  conn.close();
-    return false;
-  }
-    
-  } catch (err) {
-    console.log(err.stack);
-  }
-
-
-}
-
-
-
-async function getOriginalUrl(short){
-
-   const client = new MongoClient(dbURL,{ useNewUrlParser: true, useUnifiedTopology: true  });
-
-let conn = await client.connect();
-
-  try {
-    const db =conn.db(dbName);
-
-  
-    let r = await db.collection('urlmain').findOne({"short":parseInt(short)});
-
-      conn.close();
-   return await r;
-   }
-
-    
-   catch (err) {
-    console.log(err.stack);
-  }
-  
-
-}
-
- 
